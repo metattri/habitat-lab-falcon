2eb79787006616e42881a00944e201530b7a1be6
Zeying Gong <33891094+Zeying-Gong@users.noreply.github.com>
2024-10-14 16:49:10 +0800

    Update __init__.py

--- falcon/__init__.py
+++ falcon/__init__.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python3
-
-# Copyright (c) Meta Platforms, Inc. and its affiliates.
-# This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree.
-
 from . import rearrange_sim_v2
 from . import additional_action
 from . import additional_sensor
